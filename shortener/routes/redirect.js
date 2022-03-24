b9062f7ff43fd8a5308530c7e470ec00fe50db25
lawrence910426 <lawrence910426@gmail.com>
2022-03-25 02:45:02 +0800

    Add redis & use sequential instead of hash

--- shortener/routes/redirect.js
+++ shortener/routes/redirect.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');
+const redis = require('redis');
+const redisClient = redis.createClient();
+(async function() { await redisClient.connect(); })()
 
 var mysql = require('mysql');
 var pool  = mysql.createPool({
@@ -14,17 +18,37 @@ async function queryUrl(urlId) {
   return new Promise(res => {
     var query = pool.query(
       'SELECT url, expireAt FROM urls WHERE urlId = ?', 
-      [urlId], res
+      [urlId], (error, results, fields) => res([error, results, fields])
     );
   });
 }
 
 /* Redirect functions. */
-router.get('/:urlId', function(req, res, next) {
-  const [error, results, fields] = await queryUrl(req.params.urlId);
-  if(error) throw error;
-  if(results[0].urls.expireAt < moment().unix())
-    res.redirect(results[0].urls.url);
+router.get('/:urlId', async function(req, res, next) {
+  try {
+    var url, expireAt;
+
+    const value = await redisClient.hGetAll(req.params.urlId)
+    console.log(value)
+    if(value == undefined) {
+      const [ error, results, fields ] = await queryUrl(req.params.urlId);
+      console.log(error, results, fields)
+      if(error) throw error;
+      if(results.length == 0) throw "Link not found";
+      url = results[0].url;
+      expireAt = results[0].expireAt;
+    } else {
+      url = value.url;
+      expireAt = value.expireAt;
+    }
+
+    if(moment().unix() < expireAt)
+      res.writeHead(301, { Location: url }).end();
+    else
+      res.send("Link outdated.")
+  } catch (e) {
+    console.log(e)
+  }
 });
 
 module.exports = router;
--- shortener/routes/upload.js
+++ shortener/routes/upload.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const { createHash } = await import('crypto');
-const hash = createHash('sha512');
+const crypto = require('crypto')
+const moment = require('moment');
+const redis = require('redis');
+const redisClient = redis.createClient();
+(async function() { await redisClient.connect(); })()
 
 var mysql = require('mysql');
 var pool  = mysql.createPool({
@@ -12,22 +15,94 @@ var pool  = mysql.createPool({
   database        : process.env.dbName
 });
 
-async function insertUrl(url, expireAt, hashed) {
+redisClient.on("error", error => {
+  if(error) {
+      console.error("ERROR***",error);
+  } else {
+      console.log("Redis connect.");
+  }
+});
+
+function selectSum() {
+  return new Promise((res) => {
+    pool.query(
+      'SELECT totalRows FROM sumTable', 
+      (error, results, fields) => res([error, results, fields])
+    );
+  })
+}
+
+function updateSum() {
   return new Promise(res => {
-    var query = pool.query(
-      'INSERT INTO urls SET ?', 
-      { url: url, expireAt, expireAt, urlId: hashed }, 
-      res
+    pool.query(
+      'UPDATE sumTable SET totalRows = totalRows + 1', 
+      (error, results, fields) => res([error, results, fields])
+    );
+  })
+}
+
+function insertUrl(url, expireAt, urlId) {
+  return new Promise(res => {
+    pool.query(
+      'INSERT INTO urls (url, expireAt, urlId) VALUES (?, ?, ?)', 
+      [url, , hashed],
+      (error, results, fields) => res([error, results, fields])
     );
   });
 }
 
+function convertInt(x) {
+  var ans = ""
+  var mapping = []
+  for(var i = 0; i < 10; i++) 
+    mapping.append(String.fromCharCode('0'.charCodeAt(0) + i))
+  for(var i = 0; i < 26; i++) 
+    mapping.append(String.fromCharCode('a'.charCodeAt(0) + i))
+  for(var i = 0; i < 26; i++) 
+    mapping.append(String.fromCharCode('A'.charCodeAt(0) + i))
+  while(x > 0) {
+    ans += (x % 62)
+    x = floor(x / 62)
+  }
+  return ans;
+}
+
+async function insertTransaction(url, expireAt, urlId) {
+  return new Promise((res, rej) => {
+    pool.beginTransaction(async function(err) {
+      if(err) rej(err)
+      var [error, results, fields] = await selectSum();
+      if(error) rej(error)
+      var latest = results[0].totalRows
+      var [error, results, fields] = await updateSum();
+      if(error) rej(error)
+      var [error, results, fields] = await insertUrl(url, expireAt, urlId);
+      if(error) rej(error)
+      
+      pool.commit((err) => {
+        if (err) 
+          return pool.rollback(function() { rej(err); });
+        res(convertInt(latest));
+      });
+    })
+  })
+}
 
 /* Upload functions. */
-router.post('/urls', function(req, res) {
-  // Assume CPU is not the bottleneck
-  var hashed = hash.update(req.body.url + req.body.expireAt).digest('hex');
-  await insertUrl(req.body.url, req.body.expireAt, hashed);
+router.post('/urls', async function(req, res) {
+  try {
+    const urlId = await insertTransaction(req.body.url, expire, hashed);
+    var expire = moment(req.body.expireAt).unix();
+    res.send({
+      id: hashed,
+      shortUrl: "http://localhost/" + urlId
+    })
+    redisClient.hSet(urlId, 'url', req.body.url);
+    redisClient.hSet(urlId, 'expireAt', expire);
+    console.log(urlId, req.body.url, expire)
+  } catch (e) {
+    console.log(e)
+  }
 });
 
 module.exports = router;
